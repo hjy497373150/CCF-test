a15cd2e67745cd8e4df3134463ce9bc08f3ca9d2
hjy497373150 <497373150@qq.com>
2019-09-24 08:52:22 +0800

    revise on 9.24

--- 201709_1.cpp
+++ 201709_1.cpp
@@ -1,12 +1,11 @@
 #include<cstdio>
 int main(){
     int N,num;
-    int five_n,three_n;//分别表示买5瓶送2瓶，买3瓶送一瓶的数量
+    int five_n,three_n;//分别表示�?5瓶�?2瓶，�?3瓶送一瓶的数量
     scanf("%d",&N);
     five_n = N / 50;
     three_n = (N - 50 * five_n) / 30;
     num = five_n * 7 + three_n * 4 + (N - 50 * five_n - 30 * three_n) / 10;
     printf("%d\n",num);
     return 0;
-    
 }
